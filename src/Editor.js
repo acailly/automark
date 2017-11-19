ee4ec945973aa067d6fe46d6a7767c5f06d8243a
acailly <antoine.cailly@zenika.com>
2017-11-19 10:01:14 +0100

    :construction: Started using a Window Portal

--- src/Editor.js
+++ src/Editor.js
@@ -2,6 +2,7 @@
 import React, {Component} from 'react'
 import {isEmpty} from 'ramda'
 import StepCreator from './StepCreator'
+import WindowPortal from './WindowPortal'
 import bookmarkletFromCode from './bookmarklet/bookmarkletFromCode'
 
 class Editor extends Component {
@@ -38,6 +39,29 @@ class Editor extends Component {
     
     return (
       <div>
+        <WindowPortal>
+          <h4>Create a step</h4>
+          <StepCreator onAdd={this.onAddStep}/>
+          <h4>Steps</h4>
+          <table>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {isEmpty(renderedSteps) ? 
+                <tr>
+                  <td></td>
+                  <td>No Step</td>
+                </tr>
+                : renderedSteps}
+            </tbody>
+          </table>
+          <button onClick={this.replay}>Click to replay</button>
+          or use this <a href={this.bookmarklet()}>Bookmarklet</a>
+        </WindowPortal>
         <h4>Create a step</h4>
         <StepCreator onAdd={this.onAddStep}/>
         <h4>Steps</h4>
